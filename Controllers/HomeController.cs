2a1f5ee9487ecd9669ef86d44c232167051479c1
moritzsyl <moritz.syllaba@gmail.com>
2024-03-14 17:37:47 +0100

    Projektdateien hinzufügen.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using digitalguestbook.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace digitalguestbook.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace digitalguestbook.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
