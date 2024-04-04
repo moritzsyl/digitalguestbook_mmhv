3b4de73d82d47459f2ed20318934d08a3c11225c
moritzsyl <moritz.syllaba@gmail.com>
2024-04-04 17:48:52 +0200

    Login and Register setup

--- Program.cs
+++ Program.cs
@@ -1,8 +1,19 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using digitalguestbook.Data;
+using digitalguestbook.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("digitalguestbookDBContextConnection") ?? throw new InvalidOperationException("Connection string 'digitalguestbookDBContextConnection' not found.");
+
+builder.Services.AddDbContext<digitalguestbookDBContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<digitalguestbookUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<digitalguestbookDBContext>();
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
-//test
+builder.Services.AddControllersWithViews(); // Unterstützung von MVC hinzufügen
+
+builder.Services.AddRazorPages(); // Unterstützung von Razor Pages hinzufügen
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,5 +34,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages(); // Routen für Razor Pages hinzufügen
 
 app.Run();
